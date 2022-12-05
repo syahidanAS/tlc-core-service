bc2a51672150c564e7a4afe2251f04d0995e58b0
syahidanAS <syahidan139@gmail.com>
2022-12-05 12:29:47 +0700

    feat: get all banners

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BannerModel;
 use Illuminate\Http\Request;
 
 class BannerController extends Controller
 {
-    //
+    public function index(){
+        $result = BannerModel::get();
+        if($result){
+            return response()->json([
+                'message' => count($result) . ' Data successfully retrieved',
+                'data' => $result
+            ], 200);
+        }
+        return response()->json([
+            'message' =>'Something went wrong'
+        ], 500);
+    }
 }
